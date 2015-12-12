1d12e87db40a7b91c41aebc82744d5c49dd9e0d5
Phil Godzin <pgg2105@columbia.edu>
2015-12-12 07:33:29 -0500

    create entire playlist from mood or seed song.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var key = "I8VH1VRNONS3DZD9F";
+var mood_cache = {};
 
 /** 
 All available moods: 
@@ -25,27 +26,77 @@ $(document).ready(function(){
 $(".emoji").click(function(e){
 
 	mood = $(this).attr("mood");
-	searchByMood(mood);
-
+	$(".song-info").remove();
+	for(var i = 0; i < 10; i++){
+		createPlaylistFromMood(mood, i, .1 + i*.07);
+	}
+	createPlaylistFromSeedSong("SOUKOUX12B0B80B0BA");
 }); 
 
-function searchByMood(mood){
-	if (mood == "christmas") {
-		apiUrl = "http://developer.echonest.com/api/v4/song/search?api_key=I8VH1VRNONS3DZD9F&format=json&song_type=christmas&min_tempo=200&max_tempo=250";
-	} else {
-		apiUrl = "http://developer.echonest.com/api/v4/song/search?api_key=I8VH1VRNONS3DZD9F&format=json&mood="+mood+"&min_tempo=200&max_tempo=250";
-	}
+function createPlaylistFromSeedSong(seedId){
+	song_type_url = "http://developer.echonest.com/api/v4/song/profile?api_key=I8VH1VRNONS3DZD9F&format=json&id="+seedId+"&bucket=song_type&bucket=audio_summary";
 	$.ajax({
-		type: "GET",
-		url: apiUrl,
-		success: function(data) {
-			console.log(data);
-			var n = Math.min(15, data['response']['songs'].length);
-			var random = Math.floor((Math.random() * n));
-			id = data['response']['songs'][random]['id']
-			title = data['response']['songs'][random]['title']
-			$(".song-info").remove();
-			$("body").append('<p><span class="song-info"> id: ' + id + ' title: ' + title + ' </span>');  	
-		}
+			type: "GET",
+			url: song_type_url,
+			success: function(data) {
+				console.log(data);
+				artistId = data['response']['songs'][0]['artist_id'];
+				genreSearchUrl = "http://developer.echonest.com/api/v4/artist/terms?api_key=I8VH1VRNONS3DZD9F&id="+artistId+"&format=json";
+				$.ajax({
+					type: "GET",
+					url: genreSearchUrl,
+					success: function(artistData) {
+						genre = artistData['response']['terms'][0]['name'];
+						console.log(genre);
+						song_type = data['response']['songs'][0]['song_type'];
+						energy = data['response']['songs'][0]['audio_summary']['energy'];
+					
+						types = "";
+						for (var i = 0; i < song_type.length; i++){
+							types += "&song_type=" + song_type[i];
+						}
+						similarSongUrl = "http://developer.echonest.com/api/v4/song/search?api_key=I8VH1VRNONS3DZD9F"+types+"&style="+genre+"&min_energy="+energy+"&sort=energy-asc";
+						$.ajax({
+							type: "GET",
+							url: similarSongUrl,
+							success: function(data) {
+								console.log(data);
+							}
+						});
+
+					}
+				});
+			}
 	});
+
+}
+
+function createPlaylistFromMood(mood, i, energy){
+	if (mood in mood_cache && i < mood_cache[mood].length) {
+		id = mood_cache[mood][i]['id']
+		title = mood_cache[mood][i]['title']
+		$("body").append('<p><span class="song-info"> id: ' + id + ' title: ' + title + ' </span>');  
+	} else {
+		mood_cache[mood] = [];
+		min_energy = energy - .05;
+		max_energy = energy + .05;
+		if (mood == "christmas") {
+			apiUrl = "http://developer.echonest.com/api/v4/song/search?api_key=I8VH1VRNONS3DZD9F&format=json&song_type=christmas&min_energy="+min_energy+"&max_energy="+max_energy;
+		} else {
+			apiUrl = "http://developer.echonest.com/api/v4/song/search?api_key=I8VH1VRNONS3DZD9F&format=json&mood="+mood+"&min_energy="+min_energy+"&max_energy="+max_energy;
+		}
+		$.ajax({
+			type: "GET",
+			url: apiUrl,
+			success: function(data) {
+				console.log(data);
+				var n = Math.min(15, data['response']['songs'].length);
+				var random = Math.floor((Math.random() * n));
+				mood_cache[mood].push(data['response']['songs'][random]);
+				id = data['response']['songs'][random]['id']
+				title = data['response']['songs'][random]['title']
+				$("body").append('<p><span class="song-info"> id: ' + id + ' title: ' + title + ' </span>');  	
+			}
+		});
+	}
 };
